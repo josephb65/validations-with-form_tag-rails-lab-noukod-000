bf35cf3a891290c3b32fabd0675172a5ad8d0a86
Billy Joseph <ingbillyjoseph@gmail.com>
2019-07-31 17:30:49 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,37 @@ class PostsController < ApplicationController
   end
 
   def update
+    # @post = Post.find(params[:id])
+    # if @post.valid?
+    #   @post.update(post_params)
+    #   redirect_to post_path(@post)
+    # else
+    #   # re-render the :new template WITHOUT throwing away the invalid @post
+    #   render :edit
+    # end
+    @post = Post.find(params[:id])
+
+    @post.update(post_params)
+
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
+
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :category, :content)
+  end
+end
+    @post = Post.find(params[:id])
+
+
+  def update
     @post = Post.find(params[:id])
 
     @post.update(post_params)
